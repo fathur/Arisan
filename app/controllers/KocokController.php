b248c1973924627d290c04b8c21c1bded792e684
Fathur <fathur_rohman17@yahoo.co.id>
2015-04-23 08:49:20 +0700

    update random

--- app/controllers/KocokController.php
+++ app/controllers/KocokController.php
@@ -4,8 +4,7 @@ use Carbon\Carbon;
 
 class KocokController extends \BaseController {
 
-	protected $timeoutKocok = 10000; // dalam milisecond
-	protected $intervalKocok = 500; // dalam milisecond
+	protected $intervalKocok = 100; // dalam milisecond
 
 	function __construct()
 	{
@@ -16,7 +15,6 @@ class KocokController extends \BaseController {
 	{
 
 		return View::make('kocok.index')
-			->with('timeout', $this->timeoutKocok)
 			->with('interval', $this->intervalKocok);
 	}
 
@@ -24,7 +22,7 @@ class KocokController extends \BaseController {
 	{
 		$undian = Undian::take(1)
 			->join('members','members.id','=','undians.member_id')
-			//->where('dikocok', false)
+			->where('dikocok', false)
 			->orderBy(DB::raw('RAND()'))
 			->first();
 		return $undian;
@@ -32,9 +30,6 @@ class KocokController extends \BaseController {
 
 	public function getMenang()
 	{
-		//return [123,345,45,1213,35435,2313,4656,6786,1231,34,76,86,23,235,776,82,435,4686,11111,3423];
-		// return 2345;
-
 		$undian = Undian::take(1)
 			->join('members','members.id','=','undians.member_id')
 			->where('dikocok', false)
--- app/views/kocok/index.blade.php
+++ app/views/kocok/index.blade.php
@@ -6,12 +6,22 @@
 			<div class="number" id="number-kocok">-</div>
 
 			<div class="info hide" id="info-kocok">
-				<div class="nama" id="win-nama">Fathur Rohman</div>		
-				<div class="ba" id="win-ba">9292399.303030</div>
+				<div class="nama" id="win-nama"></div>		
+				<div class="ba" id="win-ba"></div>
 			</div>
 		</div>
 		<div class="counter-action">		
-			<button class="btn btn-lg btn-success" id="btn-kocok">Kocok</button>
+			<button class="btn btn-lg btn-success" id="btn-kocok">
+				<span class="glyphicon glyphicon-play" aria-hidden="true"></span>
+				Kocok
+			</button>
+			<div class="stop">
+				<button class="btn btn-lg btn-danger hide" id="btn-stop">
+					<span class="glyphicon glyphicon-stop" aria-hidden="true"></span>
+					Stop
+				</button>
+				
+			</div>
 			<div class="reset">
 				<a href="#" id="btn-reset" class="hide">Reset</a>
 			</div>
@@ -22,46 +32,46 @@
 
 @section('script')
 <script type="text/javascript">
+
+	var intervalId;
+
 	$('#btn-kocok').click(function() {
 		var $this = $(this);
 		$this.addClass('disabled').html('Sabar...');
 		$('#btn-reset').addClass('hide');
+		$('#btn-stop').removeClass('hide').removeClass('disabled');
 
-		var intervalIdFirst = setInterval(function(){
+		intervalId = setInterval(function(){
 			$.get("{{ URL::route('kocok.acak') }}", function(resp) {
 				console.log(resp.undian_number);
 				$('#number-kocok').html(resp.undian_number);
 			});
 		}, {{$interval}});
+	});
 
-		setTimeout(function(){
-			clearInterval(intervalIdFirst);
-		}, {{$timeout}});
+	$('#btn-stop').click(function(){
+		clearInterval(intervalId);
+		$(this).addClass('disabled');
+		$.get("{{ URL::route('kocok.menang') }}", function(resp) {
+			console.log(resp);
+			$('#number-kocok').html(resp.undian_number);
+			$('#win-nama').html(resp.member_name);
+			$('#win-ba').html(resp.member_number);
+			
+			$('#info-kocok').removeClass('hide')
+				.css({
+					'display': 'none'
+				})
+				.slideDown('slow');
+			$('#btn-kocok').html('Selesai');
+			$('#btn-reset').removeClass('hide');
 
-		{{-- Mendapatkan orang yang mendapat undian pada kocokan kali ini --}}
-		setTimeout(function(){
-			$.get("{{ URL::route('kocok.acak') }}", function(resp) {
-				console.log(resp);
-				$('#number-kocok').html(resp.undian_number);
-				$('#win-nama').html(resp.member_name);
-				$('#win-ba').html(resp.member_number);
-				
-				$('#info-kocok').removeClass('hide')
-					.css({
-						'display': 'none'
-					})
-					.slideDown('slow');
-				$this.html('Selesai');
-				$('#btn-reset').removeClass('hide');
-
-			});
-
-
-		}, {{$timeout}} + 100);
+		});
 	});
 
 	$('#btn-reset').click(function(e){
 		e.preventDefault();
+		$(this).addClass('hide');
 		$('#number-kocok').html('-');
 		$('#info-kocok').slideUp('slow', function(){
 			$(this).addClass('hide');	
@@ -69,7 +79,7 @@
 			$('#win-ba').html('');
 		});
 		$('#btn-kocok').removeClass('disabled').html('Kocok');
-		$(this).addClass('hide');
+		$('#btn-stop').addClass('hide');
 
 	});
 </script>
